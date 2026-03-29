999fa86cd47f091b734e34b39f2fd9a4e2af9f5a
Dev Js <alienwaredev369@outlook.com>
2026-03-29 14:20:21 -0300

    Pegando o resultado e colocando no parágrafo através do showResult.innerHTML

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function generateNumber() {
 
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+    const showResult = document.querySelector(".show-result")
+    showResult.innerHTML = result
     
 }
